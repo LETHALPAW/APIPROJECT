53b93d76bcb4ea6ef479b2a7e7ba899f25648a26
Yigit Alp Balli <yigitaballi@gmail.com>
2025-03-06 12:08:20 +0300

    TestimonialsController'a CRUD yöntemleri eklendi.
    `TestimonialsController` sınıfına `GET`, `POST`, `PUT` ve `DELETE` HTTP yöntemleri eklendi. Bu yöntemler, "Testimonials" ile ilgili temel CRUD işlemlerini gerçekleştirerek, her bir işlem için uygun yanıt mesajları döndürmektedir.
    
    Yeni TestimonialsController eklendi
    
    Yeni bir `TestimonialsController` sınıfı eklendi. Bu denetleyici, `Microsoft.AspNetCore.Http` ve `Microsoft.AspNetCore.Mvc` ad alanlarını kullanarak API işlevselliği sağlamak üzere yapılandırıldı. Denetleyici, `api/[controller]` rotasına sahip olup, `ControllerBase` sınıfından türetilmiştir. Bu değişiklik, API projesine yeni bir işlevsellik eklemek için temel bir yapı sunmaktadır.

---
+++ APIProjectCamp/Controllers/TestimonialsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIProjectCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController : ControllerBase
+    {
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok("Testimonials");
+		}
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			return Ok("Testimonials by id");
+		}
+		[HttpPost]
+		public IActionResult Post()
+		{
+			return Ok("Testimonials added");
+		}
+		[HttpPut]
+		public IActionResult Put()
+		{
+			return Ok("Testimonials updated");
+		}
+		[HttpDelete]
+		public IActionResult Delete()
+		{
+			return Ok("Testimonials deleted");
+		}
+	}
+}
